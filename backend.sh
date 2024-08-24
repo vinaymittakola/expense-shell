7a01663f28f6d7de95e759827fa30892e74f8595
vinaymittakola <mittakolavinay1@gmail.com>
2024-08-24 15:28:17 +0530

    functions

--- backend.sh
+++ backend.sh
@@ -36,5 +36,11 @@ VALIDATE $? "Enabling nodejs:20 version"
 dnf install nodejs -y &>>$LOGFILE
 VALIDATE $? "Installing nodejs"
 
-useradd expense
-VALIDATE $? "adding user"
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd expense &>>$LOGFILE
+    VALIDATE $? "Creating expense user"
+else
+    echo -e "Expense user already created...$Y SKIPPING $N"
+fi
